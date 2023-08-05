8d0a471f64d7b6f36ff2c78aa52b191c33e97765
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-08-05 06:58:24 +0100

    Initial version

---
+++ src/asset_sizer/__main__.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
+
+import imageio.v3 as iio
+import typer
+from PIL import Image
+
+extensions = {
+    ".png",
+    ".jpg",
+    ".jpeg",
+}
+
+sizes = []
+
+app = typer.Typer()
+
+@dataclass
+class ImageData:
+    path: Path
+    image: Image
+
+@app.command()
+def main(
+    folder: Path = typer.Argument(
+        Path("."), exists=True, file_okay=False, dir_okay=True, resolve_path=True
+    )
+):
+    images = load_images(folder)
+
+    new_width = min([ im.image.width for im in images], key=lambda width: width)
+
+    resized = resize(images, new_width)
+
+    resized_folder = folder / "resized"
+    save_files(resized, resized_folder)
+
+def save_files(resized, resized_folder):
+    resized_folder.mkdir(exist_ok=True)
+    for im in resized:
+        im.image.save(resized_folder / im.path.name)
+
+
+
+def resize(images, width):
+    resized_images: List[ImageData] = []
+    for im in images:
+        ratio = width / im.image.size[0]
+        new_height = int(im.image.size[1] * ratio)
+        new_width = int(im.image.size[0] * ratio)
+
+        resized_image = im.image.resize((new_width, new_height))
+        resized_images.append(ImageData(
+            path=im.path,
+            image=resized_image,
+        ))
+
+    return resized_images
+
+def load_images(folder: Path) -> List[ImageData]:
+    images: List[ImageData] = []
+    for file in folder.iterdir():
+        if file.is_file():
+            if file.suffix in extensions:
+                full_path = (folder / file).resolve()
+                image =  Image.fromarray(iio.imread(full_path))
+                image_data = ImageData(
+                    path=full_path,
+                    image=image,
+                )
+                images.append(image_data)
+    return images
+
+
+
+if __name__ == "__main__":
+    app()
