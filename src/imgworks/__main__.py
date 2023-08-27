a12acea5a21f67a6ea7d772129b0f59e825ae491
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-08-27 07:32:48 +0100

    Rename to imgworks

--- src/imgworks/__main__.py
+++ src/imgworks/__main__.py
@@ -1,15 +1,22 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import List, Set
 
 import imageio.v3 as iio
 import typer
 from PIL import Image
 
-extensions = {
+ORIGINAL_IMAGE_EXTENSIONS = {
     ".png",
     ".jpg",
     ".jpeg",
+}
+
+FORMATS = {
+    ".png": "png",
+    ".jpg": "jpeg",
+    ".jpeg": "jpeg",
+    ".webp": "webp",
 }
 
 sizes = []
@@ -22,28 +29,51 @@ class ImageData:
     image: Image
 
 @app.command()
-def main(
+def convert_webp(
     folder: Path = typer.Argument(
         Path("."), exists=True, file_okay=False, dir_okay=True, resolve_path=True
     ),
-    width: str  =  typer.Argument(default='min', help="Width of the resized images in pixels, or 'min' or 'max' to use the minimum or maximum width of the images in the folder")
+    to_format: str = typer.Argument(
+        "jpg",
+        help="Format to convert to",
+        show_default=True,
+    )):
+
+    images = load_images(folder, extensions={".webp"})
+
+    if not to_format.startswith("."):
+        to_format = "."+to_format
+
+    format = FORMATS.get(to_format, None)
+
+    for im in images:
+        im.image.save(im.path.with_suffix(to_format), format)
+
+@app.command()
+def resize(
+    folder: Path = typer.Argument(
+        Path("."), exists=True, file_okay=False, dir_okay=True, resolve_path=True
+    ),
+    width: str  =  typer.Argument(default="min", help=(
+        "Width of the resized images in pixels. "
+        "Use 'min' or 'max' to use the minimum or maximum width of the images in the folder"
+        )
+    )
 ):
     """Resize images in a folder to a given width"""
-    images = load_images(folder)
-
-    print(width)
+    images = load_images(folder, extensions=ORIGINAL_IMAGE_EXTENSIONS)
 
     try:
         new_width = int(width)
     except ValueError:
-        if width == 'min':
+        if width == "min":
             new_width = min([ im.image.width for im in images], key=lambda width: width)
-        elif width == 'max':
+        elif width == "max":
             new_width = max([ im.image.width for im in images], key=lambda width: width)
         else:
             raise typer.BadParameter(f"Invalid width: {width}")
 
-    resized = resize(images, new_width)
+    resized = resize_images(images, new_width)
 
     resized_folder = folder / "resized"
     save_files(resized, resized_folder)
@@ -55,7 +85,7 @@ def save_files(resized, resized_folder):
 
 
 
-def resize(images, width):
+def resize_images(images, width):
     resized_images: List[ImageData] = []
     for im in images:
         ratio = width / im.image.size[0]
@@ -70,7 +100,7 @@ def resize(images, width):
 
     return resized_images
 
-def load_images(folder: Path) -> List[ImageData]:
+def load_images(folder: Path, extensions: Set[str]) -> List[ImageData]:
     images: List[ImageData] = []
     for file in folder.iterdir():
         if file.is_file():
